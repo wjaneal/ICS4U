f84a552e7544d89845eedd3c4c853acd6e92d124
Charlotte418 <31779684+Charlotte418@users.noreply.github.com>
2018-01-04 13:53:21 -0500

    added

---
+++ 2017/python coding conventions and documentation standards.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan  4 13:00:32 2018
+
+@author: chenquancheng
+"""
+
+
+#Python coding conventions and documentation standards
+#Python coding conventions:(from Python Software Foundation)
+'''
+A tab should be used when indenting
+Imports should usually be on separate lines
+Imports are always put at the top of the file, just after any module comments and docstrings, and before module globals and constants.
+Avoid extraneous whitespace in the following situations: Immediately inside parentheses, brackets or braces;Immediately before a comma, semicolon, or colon
+Always surround these binary operators with a single space on either side: assignment (=), augmented assignment (+=, -= etc.), comparisons (==, <, >, !=, <>, <=, >=, in, not in, is, is not), Booleans (and, or, not).
+Leave one blank line between different functions
+Leave two blank lines between different classes
+'''
+#Documentation stardards:(from Python Software Foundation)
+    #(1)single-line comment: use hash tag(#)
+    #comments here...
+    #Specify the data type of a variable and what the variable records
+    #(2)multiple-line comment: use triple quotes
+    '''
+    comments here...
+    '''
+    #Comments for a function:
+    '''
+    what the function is for
+    parameters:...
+    returns:...
+    '''
+#This documentation standard makes documentation easier to write and understand
+    
+    
