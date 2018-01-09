02909013af9752d8bc3301e20212ae9eeeee4e28
zfysoom <zyfsoom@qq.com>
2018-01-09 13:55:30 -0500

    added nested loop

--- Shutupandbounce/practicebyEmma.py
+++ Shutupandbounce/practicebyEmma.py
@@ -1,7 +1,10 @@
 #coding convention
-for i in range(0,100):#This is a for loop.
+'''for i in range(0,100):#This is a for loop.
     print(i,"pigs are coming")
     if i % 10 ==5:
         print(i,"has a remainder of 5 divided by 10.")
-    
-    
+'''
+#nested loop
+for i in range(0,3):
+    for j in range(0,2):
+        print("row",i,"column",j)
