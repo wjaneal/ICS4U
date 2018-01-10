5390e1dee750a448970774aec69c00480d6c8cd6
AURORA <35112276+Winterfell3301@users.noreply.github.com>
2018-01-10 13:35:09 -0500

    Add files via upload

--- Shutupandbounce/assignment1.py
+++ Shutupandbounce/assignment1.py
@@ -1,17 +1,16 @@
-'''
-Name: Emma
-Date: Jan. 10, 2017
-Title of program: Assignment 1
-Function of program: Loop through 1000 number.
-                     If the number is divisible by 3, print"divisible by 3".
-                     If the number is divisible by 19, print"multiple by 19". 
-Variable names:i
-'''
-for i in range(0,1000):
-    if i % 3 ==0:
-        print(i, "is divisible by 3.")
-    elif i % 19 ==0:
-        print(i, "is the multiple of 19.")
+#(a) Name:Aurora Hou
+#(b) Date:2018.01.10 
+#(c) Program Title:assgenment 1 (ICS4U)
+#(d) Purpose:Loop through 1000 numbers.  Print them.
+#            if the number is divisible by 3,print"divisible by 3"
+#            if the number is divisible by 19,print"multiple of 19!" 
+#loop
+for i in range(1,1001):
+    if i % 3 == 0:
+        print("divisible by 3")
+    elif i % 19 == 0:
+        print("multiple of 19!")
     else:
         print(i)
-    
+
+
