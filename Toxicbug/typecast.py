ba94d3faad31159c929797ae58716d198e2149c2
jaune1 <35112295+jaune1@users.noreply.github.com>
2018-01-15 13:05:16 -0500

    Add files via upload

---
+++ Toxicbug/typecast.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+Name: Jaune
+Date: Jan 11
+Title: Typecasting
+Purpose: Studying
+Variable names: a,b
+This is a temporary script file.
+"""
+#Let the user input two numbers.
+a = input ("enter your number: ")
+b = input ("enter another number: ")
+#This converts the number back to an integer
+x = int(a)
+y = int(b)
+print ("The sum of your number",x,"and",y,"is",x+y)
+print ("The differnce of your number",x,"and",y,"is",x-y)
+print ("The product of your number",x,"and",y,"is",x*y)
+print ("The quotion of your number",x,"and",y,"is",x/y)
+
+#Let the user input two numbers.
+a = input ("enter your number: ")
+b = input ("enter another number: ")
+#This converts the number back to floating number
+x = float(a)
+y = float(b)
+print ("The sum of your number",x,"and",y,"is",x+y)
+print ("The differnce of your number",x,"and",y,"is",x-y)
+print ("The product of your number",x,"and",y,"is",x*y)
+print ("The quotion of your number",x,"and",y,"is",x/y)
+
+#Let the user input two numbers.
+a = input ("enter your number: ")
+b = input ("enter another number: ")
+#This converts the number back to strings
+x = str(a)
+y = str(b)
+print ("The sum of your number",x,"and",y,"is",x+y)
+print ("The differnce of your number",x,"and",y,"is",x-y)
+print ("The product of your number",x,"and",y,"is",x*y)
+print ("The quotion of your number",x,"and",y,"is",x/y)
