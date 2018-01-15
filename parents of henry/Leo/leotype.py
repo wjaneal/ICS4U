a662eccd4790c41ee5ef4d5d0d4c595d9514835b
KhanZhang <simetra.msz@gmail.com>
2018-01-15 12:55:07 -0500

    s

---
+++ parents of henry/Leo/leotype.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+# -*- coding: utf-8 -*-
+
+'''
+Name: Leo Shen <szclsya@gmail.com>
+Date: 2018-01-10
+Program Title: NumLoop
+Purpose: Loop though 1000 number and search for numbers which are divisible by 3 or 19.
+'''
+
+num1 = int(float(input()))
+num2 = int(float(input()))
+sum = num1+num2
+diff = num1 - num2
+prod = num1 * num2
+quot = num1 / num2
+
+print("The sum of a and b is "+ str(sum))
+print("The difference of a and b is " + str(diff))
+print("The product of a and b is "+ str(prod))
+print("The quotient of a and b is "+ str(quot))
