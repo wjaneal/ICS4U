9fec13b6fc850b33afe70e47985152463363f9b6
AURORA <35112276+Winterfell3301@users.noreply.github.com>
2018-01-15 12:44:23 -0500

    Add files via upload

--- Shutupandbounce/assignment2.py
+++ Shutupandbounce/assignment2.py
@@ -1,10 +1,5 @@
-'''
-Assignment 2 - Typecasting
-Name: Emma
-Title: Assignment for Typecasting
-Purpose:to write a program that converts different types of data.
-mixedCase variable names:answer1,answer2,s,d,p,q
-
+ucn#(a)assignment2
+"""
 Write a program that does the following:
 (a) Let the user input two numbers.
 (b) Convert the numbers to integers
@@ -12,35 +7,29 @@ Write a program that does the following:
 (d) Repeat (b) and (c) for floating point
 (e) Convert the numbers to strings.
 (f) Output the four results as strings as follows: The sum of a and b is 5.667, etc. for subtraction, multiplication and division.
+"""
+# 2018/01/11,Aurora Hou
+#mixedCase variable names
 
-'''
-answer1 = int(input("Plz enter the first of two integers:"))#Let the user input two numbers.
-answer2 = int(input("Plz enter the second of two integers:"))
-
-
-s=answer1+answer2
-d=answer1-answer2
-p=answer1*answer2
-q=answer1/answer2
-#Convert the numbers to integers.
-s = int(s)
-d = int(d)
-p = int(p)
-q = int(q)
-   
-#Print the sum, difference, product and quotient of the numbers.
-print(s,d,p,q)
-
-#Convert the numbers to floating point numbers.
-s = float(s)
-d = float(d)
-p = float(p)
-q = float(q)
-#Print the sum, difference, product and quotient of the numbers again.
-print(s,d,p,q)
-#Output the four results by Converting the numbers to strings.
-print("The sum of two numbers is", str(s))
-print("The difference of two numbers is", str(d))
-print("The product of two numbers is", str(p))
-print("The quotient of two numbers is", str(q))
-
+woW = (input("please input a number:  "))
+xxX = (input("please input another number:  "))
+woW = int(woW)
+xxX = int(xxX)
+print(woW + xxX)
+print(woW - xxX)
+print(woW * xxX)
+print(woW / xxX)
+woW = float(woW)
+xxX = float(xxX)
+print(woW + xxX)
+print(woW - xxX)
+print(woW * xxX)
+print(woW / xxX)
+string1 = str(woW + xxX)
+string2 = str(woW - xxX)
+string3 = str(woW * xxX)
+string4 = str(woW / xxX)
+print("The sum of two numbers is "+string1+".")
+print("The difference of two numbers is "+string2+".")
+print("The product of two numbers is "+string3+".")
+print("The quotient of two numbers is "+string4+".")
--- Shutupandbounce/assignment3.py
+++ Shutupandbounce/assignment3.py
@@ -1,131 +1,45 @@
-'''
-Assignment 3 - Typecasting
+#Aurora Hou 2018.01.12 
+#Assignment3
+#(a)
+A = input("Please input a number:")
+J = input("Now another one:")
+#(b)
+A = int(A)
+J = int(J)
+#(c)
+if A == J:
+    print("A equals to J")
+if A < J:
+    print("A is less than J")
+if A <= J:
+    print("A is less than or equals to J")
+if A > J:
+    print("A is greater than J")
+if A >= J:
+    print("A is greater than or equals to J")
+if A != J:    
+    print("A doesn't equal to J")
 
-Coding convention: 
-(a) lower case file name
-(b) Name: Emma, Date: Jan 12, Title: Assignment 3 - Typecasting,
-<<<<<<< HEAD
-Purpose:
-(c) mixedCase variable names
-
-Write a program that does the following:
-(a) Let the user input two numbers.
-(b) Convert the numbers to integers
-(c) Use six comparison operators in six different if statements to compare the two numbers.  Print different outputs for true and false in each case.
-(d) Let the user input a string
-(e) Determine whether the letters 'a', 'b', 'c', 'd' or 'e' are in the string using five diffferent if statements.  Print appropriate output in each case.
-(f) Find a way to check if each letter in the alphabet is in the string (upper and lower case) and print appropriate output. Use loops, comparison operators, decision structures and codes for the different letters of the alphabet.
-=======
-Purpose:To use comparison operators to solve problem and check whether a letter is in a word.
-(c) mixedCase variable names:a, b, u, l, word 
->>>>>>> c65b72842c94ac3d74cf98e6207d6106d0927b6d
-'''
-#(a) Let the user input two numbers and (b) convert the numbers to integers.
-a = int(input("Enter a number:"))
-b = int(input("Enter another one:"))
-#(c) Use six comparison operators in six different if statements to compare the two numbers.
-#Print different outputs for true and false in each case.
-#> < >= <= == !=
-if a>b:
-<<<<<<< HEAD
-    print("True")
-else:
-    print("False")
-if a<b:
-    print("True")
-else:
-    print("False")
-if a>=b:
-    print("True")
-else:
-    print("False")
-if a<=b:
-    print("True")
-else:
-    print("False")
-if a==b:
-    print("True")
-else:
-    print("False")
-if a!=b:
-    print("True")
-else:
-    print("False")
-#(d) Let the user input a string
-word = input("Enter a word:")
-#(e) Determine whether the letters 'a', 'b', 'c', 'd' or 'e' are in the string using five diffferent if statements.  Print appropriate output in each case.
-#(f) Find a way to check if each letter in the alphabet is in the string (upper and lower case) and print appropriate output.
-#Use loops, comparison operators, decision structures and codes for the different letters of the alphabet.
-def checkletter(word):
-    list1 = ["a","b","c","d","e"]
-    for i in range(0,len(list1)):
-        if list1(i)in word:
-            return("the letter",list1(i), " is in the word")
-        else:
-            return"not in"
-print(checkletter(word))
-
-=======
-    print(a,">",b)
-else:
-    print(a,"is not greater than",b)
-if a<b:
-    print(a,"<",b)
-else:
-    print(a,"is not smaller than",b)
-if a>=b:
-    print(a,"is greater than or equal to",b)
-else:
-    print(a,"is not greater than or equal to",b)
-if a<=b:
-    print(a,"is smaller than or equal to",b)
-else:
-    print(a,"is not smaller than or equal to",b)
-if a==b:
-    print(a,"is equal to",b)
-else:
-    print(a,"is not equal to",b)
-if a!=b:
-    print(a,"is not equal to",b)
-else:
-    print(a,"is equal to",b)
-#(d) Let the user input a string
-word = input("Enter a word:")
-#(e) Determine whether the letters 'a', 'b', 'c', 'd' or 'e' are in the
-#string using five diffferent if statements.
-#Print appropriate output in each case.
-#Use loops, comparison operators, decision structures
-#and codes for the different letters of the alphabet.
-if 'a' in word:
-    print("a is in the word.")
-else:
-    print("a is not in the word.")
-if 'b' in word:
-    print("b is in the word.")
-else:
-    print("b is not in the word.")
-if 'c' in word:
-    print("c is in the word.")
-else:
-    print("c is not in the word.")
-if 'd' in word:
-    print("d is in the word.")
-else:
-    print("d is not in the word.")
-if 'e' in word:
-    print("e is in the word.")
-else:
-    print("e is not in the word.")
-    
-for u in range(65,91):#There is a loop through uppercase letters.
-    if chr(u) in word:#(f) Check if each letter in the alphabet is in the string
-        print(chr(u)+"is in the word.")#Print appropriate output.
+#(d)
+wow = input("please input a word:")
+def checkletter(letter,word):
+    if letter in word:
+        return("The "+letter+" is in the word!")
     else:
-        print(chr(u)+"is not in the word.")
-for l in range(97,123):#There is a loop through lowercase letters.
-    if chr(l) in word:
-        print(chr(l)+"is in the word.")
+            return"The "+letter+" is not in the word!"
+print(checkletter('a',wow))
+print(checkletter('b',wow))
+print(checkletter('c',wow))
+print(checkletter('d',wow))
+print(checkletter('e',wow))
+
+lol = input("please input another word:")
+def checkletter(letter,word):
+    if letter in word:
+        return("The "+letter+" is in the word!")
     else:
-        print(chr(l)+"is not in the word.")        
-    
->>>>>>> c65b72842c94ac3d74cf98e6207d6106d0927b6d
+            return"The "+letter+" is not in the word!"
+            
+alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+for letters in range(len(alphabet)):
+    print(checkletter(alphabet[letters],lol))
