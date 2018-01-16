b8dd8930b0eec1972ae0e90bc2893778196b9b0f
Li Fengyi <1243678316@qq.com>
2018-01-17 02:58:48 +0800

    Add files

---
+++ hhh/Chris/Assignment4.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+ChrisLi
+Jan 15, 2018
+program title: Area calculator
+#program functions: This program Calculates the area under curves(mathematical functions)
+"""
+
+from math import#
+#variables
+a = 1.0 #this is the satarting x value 
+b = 7.0 #this is the end x value 
+n = 10000 #this is the number of rectangles
+area = 0.0 #this is the total area
+
+#this is the fuction to use in the area calculation
+def f(x):
+    
+     
