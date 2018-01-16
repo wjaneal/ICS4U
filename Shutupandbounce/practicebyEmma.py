c451a6aa19fe890889d29743c09e055d3759a801
zfysoom <zyfsoom@qq.com>
2018-01-15 19:50:07 -0500

    finished the a4

--- Shutupandbounce/practicebyEmma.py
+++
@@ -1,7 +0,0 @@
-#coding convention
-for i in range(0,100):#This is a for loop.
-    print(i,"pigs are coming")
-    if i % 10 ==5:
-        print(i,"has a remainder of 5 divided by 10.")
-    
-    
