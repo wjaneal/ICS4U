28486d95618ace6c793d12c9a4dd05d94ae9112a
Chrislifengyi <35307605+Chrislifengyi@users.noreply.github.com>
2018-01-25 02:24:21 +0800

    Update Assignment6.py

--- hhh/Chris/Assignment6.py
+++ hhh/Chris/Assignment6.py
@@ -11,22 +11,32 @@ Variable names: users' names answers
     A and B are the first and last initials of the user.
 """
 import time
-class quiz:#ask the user to input their name   
-    def __init__(self, full_name):
-        self.name = full_name
-        
-user1=input("user's name:")
+class quiz:
+    def __init__(self): #This part is used to start the quiz. 
+        self.score = 0 #This variable is used to record the score of the user.
+        self.name = str (input('Please write down your name: ')) #This lets user input their name.
+        name_pieces = self.name.split(' ')#This split the name of the user.
+        self.fn = name_pieces[0]#This could record the first name of the user
+        self.ln = name_pieces[-1]#This could recor the last name of the user
+        self.quiz = {"Is tokyo belongs to Japan?":"yes","Is pineapple belongs to fruits?":"yes","Who is the current president of USA?":"Trump","Is the car's speed faster than plane?":"no","Is frog belongs to human being?":"no"}
+        for i in range (0,5): #The loop structure is used here to print out the quiz questions and let users input their answers.
+            print('Question number '+str(i + 1)+' :')#This could let users know what is their current question number.
+            print(list(self.quiz.keys())[i])
+            self.answer = input('Please write your answer here: ')
+            if self.answer == list(self.quiz.values())[i]:#The decision structure here is used to determine whether the answer of the user is correct or not.
+                print('correct')
+                self.score = self.score + 1
+            else:
+                print('wrong')
+            print ('Your final score is: '+str(self.score))#This could help user know their final score.
+        self.percentage=self.score/5*100#This is used to determine the percentage of the user
+        self.saveFile()#This will save a txt file
 
-    #Dictionaries
-D = {"Is tokyo belongs to Japan?":"yes","Is pineapple belongs to fruits?":"yes","Who is the current president of USA?":"Trump","Is the car's speed faster than plane?":"no","Is frog belongs to human being?":"no"}
-answer=[]
-point=0
-Dk = list(D.keys())
-Dv = list(D.values())
-for i in D.keys():
-    answer.append(input(i))
-for i in range(0, len(Dk)):
-    if answer[i] == Dv[i]:
-        point+=20
-#show the current time, correct percentage and score of quiz        
-print (user1,"You get a score of", point,("percent: {:.2%}".format(point/100)),time.strftime("%y%m%d %H:%M:%S"))
+    def saveFile(self):#This could create a txt file and record the score of the user.
+        self.str1 = 'Quiz'+ time.strftime('%Y%m%d%H%M%S') + self.fn[0] + self.ln[0] #This is the file name.
+        self.File = open (self.str1+'.txt','w') #This is used to creat a file
+        self.str2 = 'the score of the quiz'+'\nName of the user: '+self.name+'\nScore of the quiz: '+str(self.score)+'\nPercentage of the quiz: '+str(self.percentage)#This is used to record the user's percentage, name and the score of the quiz.
+        self.File.write (self.str2)
+        self.File.close()
+        
+quiz()        
