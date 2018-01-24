c725b3fcfb7315bd481d15aac91c6df3fbbd851f
Jaune Chen <jaunechen@gmail.com>
2018-01-24 14:44:03 -0500

    s

--- Assignments/A7Jan24.py
+++ Assignments/A7Jan24.py
@@ -16,3 +16,11 @@ Rectangles, dots, text, ellipses and lines of different colours.
 (c) Blend the two together in 11 jpg photographs ranging from 0-100%, 10-90%, ... all the way to 100-0%
 
 '''
+import numpy as np
+import cv2
+img = np.zeros((512,512,3), np.uint8) #This line is to create a black image
+cv2.line(img,(0,0),(511,511),(255,0,0),5)#This
+#This line is to show the image
+cv2.imshow('image',image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
