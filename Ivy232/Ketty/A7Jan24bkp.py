350aea22b45d10a6755ebb62c3d40fc3790f0638
Ketty <s.kettyarem@gmail.com>
2018-01-26 03:05:04 +0800

    Ketty Update

--- Ivy232/Ketty/A7Jan24bkp.py
+++ Ivy232/Ketty/A7Jan24bkp.py
@@ -1,11 +1,6 @@
 #Assignment7 OpenCv
-<<<<<<< HEAD
-#Name:Mandy
-#Date: January 24, 2018
-=======
 #Name:Ketty
 #Date: January 25, 2018
->>>>>>> ketty
 '''
 Task:
 (a) Generate a jpg file with the following:
@@ -15,25 +10,20 @@ Rectangles, dots, text, ellipses and lines of different colours.
 
 (c) Blend the two together in 11 jpg photographs ranging from 0-100%, 10-90%, ... all the way to 100-0%
 '''
-<<<<<<< HEAD
-=======
+
 '''
 cv2.imshow('window_name',var) #print the var.
 cv2.waitKey(0)                #keyboard binding function.
 cv2.imwrite('file_name',var)  #save the image in PNG format in the working directory.
 cv2.destroyAllWindows()       #close all the windows we created.
 '''
->>>>>>> ketty
 import numpy as np
 import cv2
 #from matplotlib import pyplot as plt
 
 ##(a)
-<<<<<<< HEAD
-img1 = np.zeros((600,500,3), np.uint8)#creat a image(442*1284(pixels:x*y))
-=======
+
 img1 = np.zeros((600,500,3), np.uint8)#creat a image(600*500(pixels:x*y))
->>>>>>> ketty
 ##draw two lines: cv2.line(var,(x,y origin),(x,y endpoint),(Blue,Green,Red),thickness of the line)
 cv2.line(img1,(0,50),(199,50),(120,0,0),2)
 cv2.line(img1,(40,0),(100,143),(120,115,0),2)
@@ -46,17 +36,6 @@ cv2.ellipse(img1,(101,15),(261,30),0,0,180,255,-1)#draw ellipse
 ##write text
 font = cv2.FONT_HERSHEY_SIMPLEX
 cv2.putText(img1,'Rem',(100,150), font, 2,(20,100,20),1,cv2.LINE_AA)
-
-<<<<<<< HEAD
-'''
-cv2.imshow('window_name',var) #print the var.
-cv2.waitKey(0)                #keyboard binding function.
-cv2.imwrite('file_name',var)  #save the image in PNG format in the working directory.
-cv2.destroyAllWindows()       #close all the windows we created.
-'''
-=======
->>>>>>> ketty
-
 #(c)
 img2 = cv2.imread('t11.jpg')  #This is another picture.
 ##Blend the two pictures ranging from 0-100%, 10-90%, ... all the way to 100-0%.
