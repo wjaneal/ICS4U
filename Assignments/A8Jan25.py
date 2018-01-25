e9f515bb5683b4a4ed9c4c8952f09069ef5ee120
William Neal <wneal@lia-edu.ca>
2018-01-25 13:52:38 -0500

    Add Assignment 8

---
+++ Assignments/A8Jan25.py
@@ -0,0 +1,74 @@
+'''
+Assignment 8 - ImageProcessing
+
+Coding convention: 
+(a) lower case file name
+(b) Name, Date, Title, Purpose in multiline comment at the beginning
+(c) mixedCase variable names
+
+
+Get the programs in the Vision_Apps repository working.
+Take a background photo.
+Place an object in the scene.
+Take a test photo.
+Use the vision processing app to isolate the object from the photo.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
