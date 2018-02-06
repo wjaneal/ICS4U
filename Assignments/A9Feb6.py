5f3275052ff08e61f8e4e3626594b8f825b59a2b
William Neal <william.j.a.neal@gmail.com>
2018-02-06 12:44:17 -0500

    Added assignment 9

---
+++ Assignments/A9Feb6.py
@@ -0,0 +1,96 @@
+'''
+Assignment 9 - Benchmarking Algorithms
+
+
+Coding convention: 
+(a) lower case file name
+(b) Name, Date, Title, Purpose in multiline comment at the beginning
+(c) mixedCase variable names
+
+(1) Create two functions: prime1(n) and prime2(n) that take a number that represents the set of integers from which to find prime numbers.  For example, n=1000 specifies that the function will look for primes up to 1000.
+(2) Find out how to measure elapsed time in Python
+(3) Write a program that determines the amount of time taken to find primes up to the following values: n=[10000,20000,30000,40000,50000]
+(4) Output of the program:
+First algorithm:
+n	time
+10000	2.23
+20000	4.67
+30000 	7.88
+40000	11.34
+50000	14.44
+
+Second algorithm:
+n	time
+10000	2.23
+20000	14.67
+30000 	117.88
+40000	211.34
+50000	11214.44
+
+(5) Bonus:  Have Python graph the results using pyplot / matplotlib
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
