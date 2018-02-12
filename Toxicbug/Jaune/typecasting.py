81e8aa76a02d1c71b464c2e0e4b35300b428e514
Jaune Chen <jaunechen@gmail.com>
2018-02-12 18:27:54 -0500

    make some changes

--- Toxicbug/Jaune/typecasting.py
+++ Toxicbug/Jaune/typecasting.py
@@ -8,11 +8,12 @@ Purpose: Studying
 Variable names: a,b
 This is a temporary script file.
 """
-a = input ("enter your number: ")
-b = input ("enter another number: ")
-x = int(a)
-y = int(b)
-print ("The sum of your number",x,"and",y,"is",x+y)
-print ("The differnce of your number",x,"and",y,"is",x-y)
-print ("The product of your number",x,"and",y,"is",x*y)
-print ("The quotion of your number",x,"and",y,"is",x/y)
+
+a = input ("enter your number: ") #This line is to ask the operator to input a number.
+b = input ("enter another number: ") #This line is to ask the operator to input another number.
+x = int(a) #This line is to make the number integer.
+y = int(b) #This line is to make the number integer.
+print ("The sum of your number",x,"and",y,"is",x+y) #This line is to add two numbers together.
+print ("The differnce of your number",x,"and",y,"is",x-y) #This line is to subtract two numbers.
+print ("The product of your number",x,"and",y,"is",x*y) #This line is to multiply two numbers.
+print ("The quotion of your number",x,"and",y,"is",x/y) #This line is to divide two numbers.
