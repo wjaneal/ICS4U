4d699ea37bc03bf46276f5fcc8030806cbbfae24
anomomomo <anomomomo@users.noreply.github.com>
2018-02-15 22:01:45 -0500

    make a change

---
+++ Toxicbug/Jeffrey/untitled1.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 13 13:00:39 2018
+
+@author: Jeffrey
+"""
+#import module
+import random
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
