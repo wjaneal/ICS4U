017fb698e9d1bc77ab42f50a94d2d76ed63e41af
zfysoom <zyfsoom@qq.com>
2018-03-07 18:14:27 -0500

    updated the file of planning

--- FRCISP/ICSISP Planning.py
+++ FRCISP/ICSISP Planning.py
@@ -3,22 +3,24 @@
 Created on Mon Mar  5 12:53:59 2018
 
 @author: fy
-
-ICS4U ISP - FRC Project
-
-Planning Technique - Agile - Planning and test together quickly throughout all the sessions involved.
-
 Group members:Charlotte, Emma and Khan
 
+ICS4U ISP - FRC(First Robotic Competition) Project
+
+Planning Technique - Agile - Planning and test together quickly throughout all the sessions involved
+(figure out the requirements→give them definitions→start planning→start designing→implement/test→release→repair if needed).
+P.S. This is an iterative flow;Phases can overlap or repeat as needed.
+
 Content of code:
-①Framework - construction of robot.py; Organization of names of variables
-②GUI - interface of displaying the data we wre using
-③Encoder - detection of distance and time
-④Motor - movement of robot
-⑤Gyro - direction of robot
-⑥State Machine - a series of movement
-⑦Pneumatic Systems - a controller of the arms
-⑧Switches - spots of stopping
+①Framework - construction of robot.py (a base file which allows our robot to do whatever we want);
+  Organization of names of variables (will be listed below)
+②GUI - interface of displaying the data which we wre using in order to determine what the next action the robot needs to take
+③Encoder - detection of distance and time taken by the robot
+④Motor - an device which supports the movement of the robot
+⑤Gyro - determine the direction of the robot
+⑥State Machine - a series of movement provided for saving time and safety(rules in matches) concerns
+⑦Pneumatic Systems - a controller of the arms for opening and closing(grabbing or releasing an cube)
+⑧Switches - spots of stopping the elevator
 
 Variables:
 M0;M1 - Motors on the left side (control the wheels on the left side)
@@ -38,7 +40,11 @@ auto - different choices of autonomous session
 SW0 - Switch at the spot where the elevator should be stopped (lower)
 SW1 - Switch at the spot where the elevator should be stopped (higher)
 
-Mar.7 - We tested the code about Encoder and it worked.
+Timeline:
+The days before Mar.7 - We started designing the GUI(determining which data we should display);
+                        We finished coding the Gyro;
+                        We ...
+Mar.7 - We coded and tested the code about Encoder and it worked.
 
 
 
