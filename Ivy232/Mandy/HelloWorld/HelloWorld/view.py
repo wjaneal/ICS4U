87b06f0b9e2d272816da88ada66b2a15577d19a1
MandyHsu12 <t23843126@icloud.com>
2018-03-09 12:52:18 -0500

    plannin for the final project

---
+++ Ivy232/Mandy/HelloWorld/HelloWorld/view.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponse
+
+#def Hi(request):
+    #return HttpResponse("Hello, Django! I am Lamborghini.")
+
+def hello(request):
+    return HttpResponse("Hello world ! ")
+
+from django.shortcuts import render
+ 
+def hello(request):
+    context          = {}
+    context['hello'] = 'Hello World!'
+    return render(request, 'hello.html', context)
+
---
+++ Ivy232/Mandy/zqxt_views/calc/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+ 
+def add(request):
+    a = request.GET['a']
+    b = request.GET['b']
+    c = int(a)+int(b)
+    return HttpResponse(str(c))
+
+def add2(request, a, b):
+    c = int(a) + int(b)
+    return HttpResponse(str(c))
