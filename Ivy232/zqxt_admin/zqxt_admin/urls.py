f6ac107753f1a0854de3a04df33adcec7b89cada
MandyHsu12 <t23843126@icloud.com>
2018-03-09 13:47:27 -0500

    blog

--- Ivy232/zqxt_admin/zqxt_admin/urls.py
+++ Ivy232/zqxt_admin/zqxt_admin/urls.py
@@ -1,4 +1,4 @@
-"""OurWebsite URL Configuration
+"""zqxt_admin URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
@@ -13,18 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-'''
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
-'''
-from django.conf.urls import url
- 
-from . import view
- 
-urlpatterns = [
-    url('', view.hello),
 ]
