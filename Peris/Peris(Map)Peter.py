a918ed9d6c7592d56cb229678288d52262cd5f66
PeterZG <849997616@qq.com>
2018-03-09 14:06:50 -0500

    Peter's map for room names

---
+++ Peris/Peris(Map)Peter.py
@@ -0,0 +1,11 @@
+
+#Room Design
+#Peter
+#2018.3.9
+#one part of intial parts
+
+class map:
+    def _init_(self):
+        self.roomNames = ["Dungeon","OldLibrary","Courtyard","Hallway","StarRoom",
+                          "Hallway1","Courtyard2","WonderRoom","Hallway2","Ruins",
+                          "BloodZone","Hallway3","Hallway4","Balcony"]
