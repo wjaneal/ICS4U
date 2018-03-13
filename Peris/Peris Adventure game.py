581cb6c9b7790678f67fb540c539b0cd385bbbc8
Chrislifengyi <35307605+Chrislifengyi@users.noreply.github.com>
2018-03-14 01:34:55 +0800

    Add files via upload

--- Peris/Peris Adventure game.py
+++ Peris/Peris Adventure game.py
@@ -14,4 +14,92 @@
 class map:
     
     def _init_(self):      
-        self.inventory = [["rusty Crowbar"],["health potion"],["shovel"],["key"],["Keepsake ring"],["master sword"],[]]
+       self.directions = [{"N":1},{"S":0, "E":2},{"N":3,"W":1,"E":5},{"S":2,"E":4},
+                           {"W":3},{"W":2,"E":6},{"S":8,"E":7,"W":5},{"W":6},{"N":6,"W":9,"S":10},
+                           {"E":8},{"N":8,"S":11},{"S":12},{"W":13},{}]
+       self.roomNames = ["Dungeon","OldLibrary","Courtyard","Hallway","StarRoom",
+                          "Hallway1","Courtyard2","WonderRoom","Hallway2","Ruins",
+                          "BloodZone","Hallway3","Hallway4","Balcony"]
+       self.inventory = [["rusty Crowbar"],["health potion"],["shovel"],["key"],["Keepsake ring"],["master sword"],[]]
+      
+    def showinstrctions():
+        #print a main menu and the commands
+        print("Adventure game")
+        print("===============")
+        print("commands:")
+        print("'go[direction]'")
+        
+    def showstatus():
+        #print the player's current status
+        print("---------------------")
+        print("You are in the" + rooms[currentroom]["names"])
+        print("---------------------")
+        
+#a dictionary linking a room to other positions
+rooms = {
+            0 : { "names" : "Dungeon",
+                  "north" : 1} ,
+                 
+            1 : { "names" : "OldLibrary",
+                  "south" : 0,
+                  "east"  : 2} ,
+                 
+            2 : { "names" : "Courtyard",
+                  "west"  : 1,
+                  "north" : 3,
+                  "east"  : 5} , 
+            
+            3 : { "names" : "Hallway",
+                  "south" : 3,
+                  "east"  : 4} ,
+                 
+            4:  { "names" : "StarRoom",
+                  "west"  : 3} ,
+            
+            5:  { "names" : "Hallway1",
+                  "west"  : 2,
+                  "east"  : 6} ,
+                 
+            6:  { "names" : "Courtyard2",
+                  "west"  : 5,
+                  "east"  : 7,
+                  "south" : 8} ,
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
