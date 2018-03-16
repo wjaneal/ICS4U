b697856524e0aa7bf823474d29b54134d7a7644a
LilyTao <bravoLavier@gmail.com>
2018-03-16 11:11:42 -0400

    haha

---
+++ Ivy232/Lily/hippo/polls/views.py
@@ -0,0 +1,53 @@
+from django.shortcuts import get_object_or_404, render
+
+from django.http import Http404
+
+from django.http import HttpResponse
+'''
+def index(request):
+    return HttpResponse("Byebye, world. You're at the polls index.")
+'''
+def detail(request, question_id):
+   ##### return HttpResponse("You're looking at question %s." % question_id)
+  
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+    
+'''
+  
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
+'''
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
+from django.template import loader
+from .models import Question
+
+'''
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+'''
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+'''   
+ output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+'''
+# Create your views here.
