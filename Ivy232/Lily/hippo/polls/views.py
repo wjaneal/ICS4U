82351f95c2b644ac05c4012ad1d67275368d7be5
LilyTao <bravoLavier@gmail.com>
2018-03-19 11:24:20 -0400

    aaa

--- Ivy232/Lily/hippo/polls/views.py
+++ Ivy232/Lily/hippo/polls/views.py
@@ -2,7 +2,29 @@ from django.shortcuts import get_object_or_404, render
 
 from django.http import HttpResponseRedirect, HttpResponse
 
+from django.views import generic
 
+from django.urls import reverse
+
+from .models import Choice, Question
+
+from django.template import loader
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+'''
 def detail(request, question_id):
    ##### return HttpResponse("You're looking at question %s." % question_id)
   
@@ -14,7 +36,7 @@ def detail(request, question_id):
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/results.html', {'question': question})
-
+'''
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -31,14 +53,11 @@ def vote(request, question_id):
         selected_choice.save()
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
 
-from django.template import loader
 
-from django.urls import reverse
-
-from .models import Choice, Question
-
+'''
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
+'''
 # Create your views here.
