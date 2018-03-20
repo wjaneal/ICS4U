c39e2400652917bebaf3a0b2aeb9492be3f7b651
LilyTao <bravoLavier@gmail.com>
2018-03-19 21:10:58 -0400

    improve views

--- Ivy232/Lily/group_work_django/ResultPhotoRaw.py
+++ Ivy232/Lily/group_work_django/ResultPhotoRaw.py
@@ -15,13 +15,8 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """
-    Return the last five published questions (not including those set to be
-    published in the future).
-    """
-        return Question.objects.filter(
-            pub_date__lte=timezone.now()
-    ).order_by('-pub_date')[:5]
+        ###Return the last five published questions.
+        return Question.objects.order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Question
