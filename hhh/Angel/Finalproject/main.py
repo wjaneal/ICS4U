871f45ce94e889af11b69ac66826c03d12b18e24
AngelKan <31551825+AngelKan@users.noreply.github.com>
2018-03-27 12:47:05 -0400

    .

--- hhh/Angel/Finalproject/main.py
+++ hhh/Angel/Finalproject/main.py
@@ -4,10 +4,10 @@ import random
 questions=list(sciencefact.SF.keys())
 answers=list(sciencefact.SF.values())
 
+class quiz:
 
-
-#basic and main part of creating multiple unrepeated answers
-def choosea(numofque):
+    #basic and main part of creating multiple unrepeated answers
+    def createa(numofque):
         allChoices=['Indonesia','Asia','Israel','India','Russia','China',"Japan"]
         choices=[0,1,2,3]
         #set correct answer
@@ -33,11 +33,11 @@ def choosea(numofque):
         for element in choices:
             #print(element,"start one by one")
             if element== 0 or element== 1 or element==2 or element==3:
-                rand(numbers,i,choices,allChoices,element)
+                quiz.rand(numbers,i,choices,allChoices,element)
         return choices
 
-#choose answers that have not been selected before.If they have, choose again
-def rand(numbers,i,choices,allChoices,element):
+    #choose answers that have not been selected before.If they have, choose again
+    def rand(numbers,i,choices,allChoices,element):
             num= random.randrange(0,7)# chhose a wrong answer from all the possible answers
             #print(num,"pick one from answers")
             if num != i and num not in numbers:#if not repeated
@@ -46,38 +46,52 @@ def rand(numbers,i,choices,allChoices,element):
                 choices[choices.index(element)] = allChoices[num]#put the wrong answer
                 #print(num,i,choices)
             else:#if repeated,run again
-                rand(numbers,i,choices,allChoices,element)
+                quiz.rand(numbers,i,choices,allChoices,element)
 
 
-#select a question from the dictionary
-def selectq(qs):
+    #select a question from the dictionary
+    def selectq(qs):
         numofque=random.randrange(0,len(questions))#choose a question
         if numofque not in qs:
             qs.append(numofque)
         else:# if repeated
-            selectq(qs)
+            quiz.selectq(qs)
 
-def action(qs):
-        selectq(qs)
-        numofque=qs[-1]
-        choices=choosea(numofque)
-        print(questions[numofque])
-        print(choices)
-        item=int(input("your answer:"))
-        judge(numofque,item,choices)
 
-#show if the answer is correct
-def judge(numofque,item,choices):
-        if choices[item]==answers[numofque]:
+    #show if the answer is correct
+    def judge(numofque,item,choices):
+        if choices[item-1]==answers[numofque]:
             print("correct!")
+            quiz.ra+=1
         else:
             print("wrong!")
+        
 
-#run for n times
-def loop(n):
+
+    ra=0
+    
+    #run for n times
+    def loop(n):
         qs=[]
         for i in range(n):
-            action(qs)
+                print('current:',i+1,'/',n)
+                print('correct:',quiz.ra,'/',n)
+                quiz.selectq(qs)
+                numofque=qs[-1]
+                choices=quiz.createa(numofque)
+                print(questions[numofque])
+                print(choices)
+                item=int(input("your answer:"))
+                quiz.judge(numofque,item,choices)
 
-loop(10)
-    
+        print('your correct percemtage:',quiz.ra/n*100,'%')
+
+print('the following questions all come from www.factslides.com')
+print('they do not support personal opinions')
+print('')
+print('input 1,2,3,or 4 when required')
+print('')
+quiz.loop(3)
+
+
+
