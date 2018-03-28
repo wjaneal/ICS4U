4c8f87acff88103a41f120057db096839c94be65
yingjiex <31779709+yingjiex@users.noreply.github.com>
2018-03-28 12:53:12 -0400

    a

--- FlyingSausage/Transaction and Blockchain Part (Mike and Jeffery)/encryption1.py
+++ FlyingSausage/Transaction and Blockchain Part (Mike and Jeffery)/encryption1.py
@@ -5,25 +5,26 @@ Created on Thu Mar 22 13:41:58 2018
 @author: Mike + Jeff
 """
 
-import math
 class Basic:
     
     def convert_to_binary(self,originString):
+        self.originString = str(input(' '))
         self.binaryString = ' '.join(format(ord(x),'b')for x in self.originString)
+        print(self.binaryString)
         return self.binaryString
     
     def binary_to_chaos(self,binaryString):
-        self.divide = math.floor(len(binaryString)/5)
-        self.part1 = binaryString[:self.divide]
-        self.part2 = binaryString[self.divide:2*self.divide]
-        self.part3 = binaryString[2*self.divide:3*self.divide]
-        self.part4 = binaryString[3*self.divide:4*self.divide]
-        self.part5 = binaryString[4*self.divide:]
+        self.listB = []
+        self.listB = self.binaryString.split(' ')
+        return self.listB
         
-    
     def chaos_to_binary(self,chaosString):
         pass
     
     def binary_to_orgin(self,binaryString):
         pass
     
+
+Basic.convert_to_binary()
+
+#Basic.binary_to_chaos()
