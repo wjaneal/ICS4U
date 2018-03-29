040ac40f2c0ff0d175fbb854827b323e764fbfac
MandyHsu12 <t23843126@icloud.com>
2018-03-28 20:14:49 -0400

    git commit -m

--- Ivy232/Mandy/mysite/polls/models.py
+++ Ivy232/Mandy/mysite/polls/models.py
@@ -1,16 +1,28 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
 class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    question_text = models.CharField(max_length=200) # CharField for character fields
+    pub_date = models.DateTimeField('date published') # DateTimeField for datetimes. 
 
-
+    def __str__(self): # print the objects(questions).
+        return self.question_text
+    
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    
 class Choice(models.Model):
-    question = models.ForeignKey(
-        'Question',
-        on_delete=models.DO_NOTHING,
-        )
+    question = models.ForeignKey(Question, on_delete=models.CASCADE) # ForeignKey tells Django each Choice is related to a single Question.
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+    
+    def __str__(self): # print the objects(choices/answers).
+        return self.choice_text
+
+def was_published_recently(self):
+    now = timezone.now()
+    return now - datetime.timedelta(days=1) <= self.pub_date <= now
--- Ivy232/zqxt_admin/blog/models.py
+++ Ivy232/zqxt_admin/blog/models.py
@@ -2,9 +2,23 @@ from django.db import models
 
 # Create your models here.
  
-class Article(models.Model):
-    title = models.CharField('Tielw', max_length=256)
+class Article(models.Model):# Basic information about the articles.
+    title = models.CharField('Title', max_length=256)
     content = models.TextField('Content')
  
     pub_date = models.DateTimeField('PublishDate', auto_now_add=True, editable = True)
     update_time = models.DateTimeField('ApdateDate',auto_now=True, null=True)
+
+    def __str__(self): # Model: Give articles names
+        return self.title
+'''
+class Person(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+ 
+    def my_property(self):
+        return self.first_name + ' ' + self.last_name
+    my_property.short_description = "Full name of the person"
+ 
+    full_name = property(my_property)
+'''
