675aba2da799290af6ccdf31abe6dfe8e246b3f1
LilyTao <bravoLavier@gmail.com>
2018-04-03 20:09:24 -0400

    upload

--- Ivy232/blog__的副本1/post/models.py
+++ Ivy232/blog__的副本1/post/models.py
@@ -10,7 +10,7 @@ class Post(models.Model):
  
     published_date = models.DateTimeField(
             blank=True, null=True)
-  
+
  
     def publish(self):
         self.published_date = timezone.now()
