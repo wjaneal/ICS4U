f03da2070416de5bb9c5c9e835fc80d63c5efcfa
AngelKan <31551825+AngelKan@users.noreply.github.com>
2018-04-11 14:17:28 -0400

    .

--- hhh/Angel/Finalproject/client.py
+++ hhh/Angel/Finalproject/client.py
@@ -1,7 +1,8 @@
 import socket
+import main
 
 # create a socket object
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
 
 # get local machine name
 host = socket.gethostname()                           
@@ -9,11 +10,34 @@ host = socket.gethostname()
 port = 9999
 
 # connection to hostname on the port.
-s.connect((host, port))                               
+clientsocket.connect((host, port))                               
 
-# Receive no more than 1024 bytes
-tm = s.recv(1024)                                     
+run = ""
+while run !="Q":
+    ra=0# number of correct answers
+    main.start()#print notice
+    n = str(input("how many questions?"))
+    clientsocket.send(n.encode())#send number of questions to server
+    for i in range(int(n)):
+        print("current:"+str(i+1)+"/"+n)#current question of total question
+        print("correct:"+str(ra)+"/"+n)# correct answers of total questions
+        # Receive no more than 1024 bytes
+        qac = clientsocket.recv(1024)#receive question and choices
+        if not qac:
+            break
+        print(qac)
 
-s.close()
+        item=str(input("your answer:"))#let user input answer
+        clientsocket.send(item.encode())#send answer to server
 
-print("The time got from the server is %s" % tm.decode('ascii'))
+        result= clientsocket.recv(1024)#receive result from server
+        if not result:
+            break
+        print(result.decode())
+        if result.decode()=="correct!":
+            ra+=1#number of correct answers+1
+    print("Your correct percentage:"+str(ra/int(n)*100)+'%')#print percentage
+    run = input("Q to quit")#press Q to quit, other buttons to play again
+clientsocket.close()
+
+
--- hhh/Angel/Finalproject/server.py
+++ hhh/Angel/Finalproject/server.py
@@ -1,5 +1,9 @@
 import socket
-import time
+import main
+import sciencefact
+
+questions=list(sciencefact.SF.keys())
+answers=list(sciencefact.SF.values())
 
 # create a socket object
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
@@ -16,10 +20,31 @@ serversocket.bind((host, port))
 serversocket.listen(5)                                           
 
 while True:
+    print ("waiting for connection...")
+    
     # establish a connection
     clientsocket,addr = serversocket.accept()      
-
     print("Got a connection from %s" % str(addr))
-    currentTime = time.ctime(time.time()) + "\r\n"
-    clientsocket.send(currentTime.encode('ascii'))
+
+    qs=[]#order number of question selected in dictionary
+    n = clientsocket.recv(1024).decode()
+    if not n:
+        break
+    for i in range(int(n)):
+        main.quiz.selectq(qs) #select questions
+        numofque=qs[-1]#the last question of all the selected ones
+        choices=main.quiz.createa(numofque)#create answers to the question
+        qac=questions[numofque]+" 1."+choices[0]+" 2."+choices[1]+" 3."+choices[2]+" 4."+choices[3]
+        clientsocket.send(qac.encode())#send question and choices to client
+
+        item = clientsocket.recv(1024).decode()#receive the user choice
+        if not item:
+            break
+
+        result="wrong!"
+        if choices[int(item)-1]==answers[numofque]:#if correct
+            result="correct!"
+        clientsocket.send(result.encode())#send wrong if the answer is wrong, send correct if correct
+
     clientsocket.close()
+serversocket.close()
